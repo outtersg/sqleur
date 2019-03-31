a69abb81fdb1af9784894aff8dfc37224fcb68d4
Guillaume Outters <guillaume-sqleur@outters.eu>
2019-03-31 01:03:14 +0100

    SqleurPreproExpr: gestion des listes
    + On gère carrément vachement mieux tout ce qui est listes. En fait avant on ne gérait juste pas du tout.
    = Et on ne pète tout de même pas la (désormais tordue) syntaxe "machin in truc, bidule" sans parenthèses autour de "truc, bidule".
    # L'analyseur, qui avait été prévu pour travailler sur différents plans (d'abord regrouper les chaînes entre guillemets, puis découper sur les parenthèses, puis sur les opérateurs) avait tout mis sur un seul plan.
    
    darcs-hash:3926862a82025fb0dcf31c5c4afecbcecaf570e5

--- SqleurPreproExpr.php
+++ SqleurPreproExpr.php
@@ -23,11 +23,19 @@
 
 class SqleurPreproExpr
 {
+	public static $Ops = array
+	(
+		'and', '&&', 'or', '||',
+		'==', '!=',
+		'+', '-', '*', '/',
+		'in',
+	);
+	
 	public function decouper($expr)
 	{
 		$bouts = array();
 		
-		preg_match_all('# +|[,"!/]|==#', $expr, $découpe, PREG_OFFSET_CAPTURE);
+		preg_match_all('# +|[,"!/()]|==#', $expr, $découpe, PREG_OFFSET_CAPTURE);
 		$pos = 0;
 		foreach($découpe[0] as $découpé)
 		{
@@ -51,6 +59,65 @@ class SqleurPreproExpr
 		
 		return $racine;
 	}
+	
+	/* NOTE: ,
+	 * La virgule peut intervenir sur deux plans.
+	 * Classiquement, elle est sur un plan prioritaire. Ainsi:
+	 *   fonction(machin or truc, bidule) 
+	 * se groupe:
+	 *   fonction({machin or truc}, {bidule})
+	 * Cependant, après le mot-clé "in", on accepte une liste à virgules (historiquement pour une question de simplicité d'écriture).
+	 * Ainsi:
+	 *   fonction(machin in "truc", "bidule")
+	 * se lit:
+	 *   fonction({machin} in {"truc", "bidule"})
+	 * et non:
+	 *   fonction({machin in "truc"}, {"bidule"})
+	 * Pour gérer cette particularité, la virgule est placée sur un plan prioritaire; lorsqu'elle est appelée dans ce plan, elle regarde ce qui la précède, et, si elle trouve un "in", passe son tour, pour se faire retraiter plus tard sur le plan moins prioritaire.
+	 * N.B.: afin de rester sur la simplicité d'une analyse par la gauche, on ne gérera que les cas simples: "… in <mot>, <mot>, <mot>" ne sera compris que pour des <mot>s simples, et non des expressions. Ainsi, "var in 3 * 2, 4" sera interprété comme "((var in 3) * 2), 4". Si vous n'êtes pas contents utilisez des parenthèses.
+	 * N.B.: l'analyse se fait avant arborisation de la partie gauche, afin que la virgule de "a not in 1, 2" détecte l'"in" dans sa partie gauche brute; si on arborait, le not deviendrait la racine de la partie gauche à la place du in.
+	 */
+	public static $Prios = array
+	(
+		array
+		(
+			'"' => 'chaîne',
+			'/' => 'chaîne',
+		),
+		array
+		(
+			'(' => '(',
+			')' => ')',
+		),
+		array
+		(
+			',' => ',',
+		),
+		array
+		(
+			'!' => 'devant',
+			'not' => 'devant',
+			'and' => 'bi',
+			'or' => 'bi',
+		),
+		array
+		(
+			'in' => 'bimulti',
+			',,' => ',', // La , du in, créée par celle de niveau plus haut.
+			'==' => 'bi',
+			'!=' => 'bi',
+		),
+		array
+		(
+			'defined' => 'devant',
+		),
+	);
+	public static $Fermantes = array
+	(
+		'(' => ')',
+		'[' => ']',
+		'{' => '}',
+	);
 	
 	public function arborer($bouts, $positions = null, $exprComplète = null)
 	{
@@ -111,29 +178,20 @@ class SqleurPreproExpr
 	
 	public function _arborer($bouts, $positions, $exprComplète)
 	{
-		$recherchés = array
-		(
-			array
-			(
-				',' => 'bi',
-				'!' => 'devant',
-				'not' => 'devant',
-				'defined' => 'devant',
-				'in' => 'bimulti',
-				'==' => 'bi',
-				'"' => 'chaîne',
-				'/' => 'chaîne',
-			),
-		);
-		
-		foreach($recherchés as $plan => $recherchésPlan)
+		foreach(static::$Prios as $plan => $recherchésPlan)
+		{
 			foreach($bouts as $num => $bout)
 				if(is_string($bout) && isset($recherchésPlan[$bout]))
 				{
 					switch($recherchésPlan[$bout])
 					{
+						case ',':
 						case 'bimulti':
 						case 'bi':
+							// Détection du cas particulier de la , du in.
+							if($this->_virguleRétrogradée($bouts, $num))
+								continue;
+							// Autres cas.
 							$racine = new NœudPrepro($bout);
 							$fils = array(array_slice($bouts, 0, $num), array_slice($bouts, $num + 1));
 							if(isset($positions))
@@ -143,19 +201,49 @@ class SqleurPreproExpr
 								$fil = $this->arborer($fil, isset($positionsFils) ? $positionsFils[$numFil] : null, $exprComplète);
 								if(is_array($fil))
 								{
+									// À FAIRE: permettre la dernière virgule vide.
 									if(count($fil) != 1)
-										throw new Exception('L\'opérateur binaire '.$bout.' attend deux membres de part et d\'autre');
+										throw new ErreurExpr('L\'opérateur binaire '.$bout.' attend deux membres de part et d\'autre; reçu à '.($numFil ? 'droite' : 'gauche').":\n".print_r($fil, true), $positions, $num);
 									$fil = array_shift($fil);
 								}
 								$racine->f[] = $fil;
 							}
-							if($recherchésPlan[$bout] == 'bimulti')
-								$racine->f[1] = $this->listerVirgules($racine->f[1]);
+							switch($recherchésPlan[$bout])
+							{
+								case ',':
+									// Les virgules successives sont agrégées.
+									if(is_object($racine->f[1]) && $racine->f[1] instanceof NœudPrepro && in_array($racine->f[1]->t, array(',', ',,')))
+										array_splice($racine->f, 1, 1, $racine->f[1]->f);
+									break;
+								case 'bimulti':
+									// Les opérateurs qui acceptent à droite une liste à virgules la voient transformée en tableau.
+									if(is_array($racine->f[1])) {}
+									else if(is_object($racine->f[1]) && $racine->f[1] instanceof NœudPrepro && in_array($racine->f[1]->t, array(',,', ','))) // Les deux types de liste conviennent: soit « toto in "a", "b" » dont la virgule se sera transformée en ,, lorsqu'elle se sera vue suivant un bimulti; soit « toto in ("a", "b") » qui aura généré de la vraie virgule (plus exactement une parenthèse contenant de la virgule, mais la parenthèse se sera effacée au profit de son contenu).
+										$racine->f[1] = $racine->f[1]->f;
+									else
+										$racine->f[1] = array($racine->f[1]);
+									break;
+							}
 							return $racine;
 						case 'devant':
 							$this->_splice($bouts, $positions, $num, 1);
 							$racine = new NœudPrepro($bout, $this->arborer($bouts, $positions, $exprComplète));
 							return $racine;
+						case ')':
+							// On ne s'embête pas: l'arboraison sera faite par la parenthèse ouvrante correspondante.
+							$racine = new NœudPrepro($bout, array(array_slice($bouts, 0, $num), array_slice($bouts, $num + 1)));
+							return $racine;
+						case '(':
+							$dedansEtAprès = $this->arborer(array_slice($bouts, $num + 1), $positions ? array_slice($positions, $num + 1) : null, $exprComplète);
+							if(!is_object($dedansEtAprès) || ! $dedansEtAprès instanceof NœudPrepro || $dedansEtAprès->t != static::$Fermantes[$bout])
+								throw new ErreurExpr($bout.' sans son '.static::$Fermantes[$bout], $positions, $num);
+							$dedans = new NœudPrepro($bout, $this->arborer($dedansEtAprès->f[0]));
+							$après = $dedansEtAprès->f[1];
+							$this->_splice($bouts, $positions, $num, count($bouts), array_merge(array($dedans), $après));
+							// On ne partira pas d'ici sans avoir déterminé l'usage de cette parenthèse: ouvre-t-elle une liste d'arguments de fonction, ou bien sert-elle simplement à regrouper des trucs pour une question de priorité d'opérateurs?
+							$this->_usageParenthèse(/*&*/ $bouts, /*&*/ $positions, /*&*/ $num);
+							// Réarborons le tout!
+							return $this->arborer($bouts, $positions, $exprComplète);
 						case 'chaîne':
 							$chaînes = array();
 							for($fin = $num; ++$fin < count($bouts);)
@@ -182,9 +270,10 @@ class SqleurPreproExpr
 							return $this->arborer($bouts, $positions, $exprComplète);
 					}
 				}
+		}
 		$trucs = array();
-		foreach($bouts as $truc)
-			if(is_object($truc))
+		foreach($bouts as $num => $truc)
+			if(is_object($truc) || is_array($truc))
 				$trucs[] = $truc;
 			else if(strlen(trim($truc)))
 				$trucs[] = new NœudPrepro('mot', $truc);
@@ -251,12 +340,91 @@ class SqleurPreproExpr
 		if(!($expr instanceof NœudPrepro))
 			throw new Exception('Truc improbable après une virgule');
 		
-		if($expr->t != ',')
+		if(!in_array($expr->t, array(',', ',,')))
 			return array($expr);
 		
 		$r = array_merge(array($expr->f[0]), $this->listerVirgules($expr->f[1]));
 		
 		return $r;
+	}
+	
+	protected function _virguleRétrogradée(& $bouts, $num)
+	{
+		// Bon déjà celui-ci est-il une virgule?
+		if(!is_string($bouts[$num]) || $bouts[$num] != ',')
+			return false;
+		// Le précédent (hors espaces) est-il un mot simple?
+		if(($numPréc = $this->_précédentNonVide($bouts, $num, static::PREC_MOT_SIMPLE)) === false)
+			return false;
+		// Le pénultième est-il un bimulti (ou bien une ",," précédemment posée)?
+		if(($numPréc = $this->_précédentNonVide($bouts, $numPréc, static::PREC_BIMULTI)) === false)
+			return false;
+		// C'est gagné!
+		$bouts[$num] = ',,';
+		return true;
+	}
+	
+	/**
+	 * S'assure qu'une parenthèse a un usage parmi ceux identifiés (soit pour ouvrir la liste de paramètres d'une fonction, avec des virgules dedans, soit pour constituer un groupe, sans virgule (ex.: "truc or (machin and bidule)")).
+	 */
+	protected function _usageParenthèse(& $bouts, & $positions, & $num)
+	{
+		$bout = $bouts[$num];
+		if(!is_object($bout) || ! $bout instanceof NœudPrepro || $bout->t != '(')
+			throw new Exception("_usageParenthèse() appelée sur un nœud non parenthèse");
+		if(($numPréc = $this->_précédentNonVide($bouts, $num, static::PREC_SAUF_OP)) !== false)
+		{
+			$nœudFonction = new NœudPrepro('f', array($bouts[$numPréc], $bout));
+			if(is_object($nœudFonction->f[1]) && $nœudFonction->f[1] instanceof NœudPrepro && $nœudFonction->f[1]->t == '(')
+				$nœudFonction->f[1] = $nœudFonction->f[1]->f;
+			if(is_object($nœudFonction->f[1]) && $nœudFonction->f[1] instanceof NœudPrepro && $nœudFonction->f[1]->t == ',')
+				$nœudFonction->f[1] = $nœudFonction->f[1]->f;
+			$this->_splice($bouts, $positions, $numPréc, $num - $numPréc + 1, array($nœudFonction));
+			$num = $numPréc;
+		}
+		else if(count($bout->f) == 1)
+			$this->_splice($bouts, $positions, $num, 1, array($bout->f));
+		else
+			throw new ErreurExpr("Erreur interne: parenthèse ouvrante qui n'est ni fonction, ni regroupement d'expressions", $positions, $num);
+	}
+	
+	const PREC_MOT_SIMPLE = 0x01;
+	const PREC_BIMULTI    = 0x02;
+	const PREC_SAUF_OP    = 0x04;
+	
+	protected function _précédentNonVide($bouts, $num, $mode)
+	{
+		while(--$num >= 0)
+		{
+			$bout = $bouts[$num];
+			
+			// Chaîne vide, ne compte pas.
+			if(is_string($bout) && !trim($bout))
+				continue;
+			
+			// Un des critères suffit.
+			if($mode & static::PREC_SAUF_OP) // Le bimulti (mot-clé "in" par exemple) est considéré comme un opérateur.
+				if((is_string($bouts[$num]) && !$this->_estBimulti($bouts[$num]) && !in_array($bouts[$num], self::$Ops)) || (is_object($bouts[$num]) && $bouts[$num] instanceof NœudPrepro && $bouts[$num]->t != 'op'))
+					return $num;
+			if($mode & static::PREC_MOT_SIMPLE)
+				if(is_string($bout) || (is_object($bout) && $bout instanceof NœudPrepro && $bout->t == '"'))
+					return $num;
+			if($mode & static::PREC_BIMULTI)
+				if($bout == ',,' || $this->_estBimulti($bout))
+					return $num;
+			
+			// Sinon c'est une chaîne non vide, mais qui ne répond à aucun des critères.
+			return false;
+		}
+		return false;
+	}
+	
+	protected function _estBimulti($bout)
+	{
+		foreach(static::$Prios as $symbolesNiveau)
+			foreach($symbolesNiveau as $symbole => $cat)
+				if($symbole === $bout)
+					return $cat == 'bimulti';
 	}
 	
 	public function calculer($expr, $contexte)
