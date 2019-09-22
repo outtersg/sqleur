fdb667e4416e8ab40299a7bb01cd295c5c5670a8
Guillaume Outters <guillaume-sqleur@outters.eu>
2019-09-22 17:15:16 +0200

    SqleurTestSuite
    + Exemple (voire socle) de test PHPUnit travaillant autour d'un SQL.
    
    darcs-hash:0650726f7f5b25438f45b3a19657afe995ab37e2

---
+++ SqleurTestSuite.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Copyright (c) 2019 Guillaume Outters
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+require_once dirname(__FILE__).'/Sqleur.php';
+require_once dirname(__FILE__).'/SqleurPreproIncl.php';
+require_once dirname(__FILE__).'/SqleurPreproTest.php';
+require_once dirname(__FILE__).'/SqleurPreproPif.php';
+
+/**
+ * Classe-socle pour un test PHPUnit.
+ */
+class SqleurTestSuite extends \PHPUnit\Framework\TestCase
+{
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$prépros = array
+		(
+			'i' => new SqleurPreproIncl(),
+			't' => new SqleurPreproTest(SqleurPreproTest::PHPUNIT),
+			'p' => new SqleurPreproPif(),
+		);
+		$this->sqleur = new Sqleur(array($this, 'sortir'), $prépros);
+	}
+	
+	public function sql()
+	{
+		$infosClasse = new ReflectionClass($this);
+		return strtr($infosClasse->getFileName(), array('.php' => '.sql'));
+	}
+	
+	public function testSql()
+	{
+		$this->sqleur->decoupeFichier($this->sql());
+	}
+	
+	public function sortir($req)
+	{
+		$r = $this->bdd()->prepare($req);
+		$r->execute();
+		return $r;
+	}
+}
+
+?>
