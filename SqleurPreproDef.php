9e4c95279c7ee988756cdc40645dada02d75d672
Guillaume Outters <guillaume-sqleur@outters.eu>
2020-02-10 21:25:00 +0100

    #set
    + Comme un #define, mais gère des expressions (ex.: #set COMPTEUR COMPTEUR + 1).
    
    darcs-hash:8291cc15811a95c7ecc596ec001610b41ff2758a

---
+++ SqleurPreproDef.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright (c) 2020 Guillaume Outters
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+include_once 'SqleurPreproExpr.php';
+
+class SqleurPreproDef
+{
+	protected $_préfixes = array('#calc', '#set');
+	
+	public function préprocesse($motClé, $directiveComplète)
+	{
+		if(!in_array($motClé, $this->_préfixes))
+			return false;
+		
+		$déf = preg_split('/[ 	]+/', $directiveComplète, 3);
+		$var = $déf[1];
+		$val = isset($déf[2]) ? $déf[2] : '';
+		
+		if(!in_array($motClé, array('#define')))
+		{
+			$e = new SqleurPreproExpr();
+			$val = $e->calculer($val, $this->_sqleur);
+		}
+		else
+			$val = $this->_sqleur->appliquerDéfs($val);
+		
+		$this->_sqleur->ajouterDéfs(array($var => $val));
+	}
+}
+
+?>
