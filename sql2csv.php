771fbcc805601840d3347bfa54f58818401d8aae
Guillaume Outters <guillaume-sqleur@outters.eu>
2020-02-14 00:30:33 +0100

    sql2csv: stdout se fiche du nombre de colonnes
    # Le message "Attention, vous n'avez pas le même nombre de colonnes" est pertinent pour un export CSV, mais pas pour un affichage texte simple.
    
    darcs-hash:7a221838eeb03d3075546ad15e06f8de90a4ffca

--- sql2csv.php
+++ sql2csv.php
@@ -177,6 +177,7 @@ class JoueurSql extends Sqleur
 		foreach($this->sortiesDéjàUtilisées as $sortieAncienne)
 			if($sortieAncienne->descr === $this->sortie->descr)
 			{
+				if($this->sortie->descr !== Flux::STDOUT)
 				fprintf(STDERR, '# La sortie "'.$sortieAncienne->descr.'" est réutilisée en ayant déjà servi pour l\'export d\'une autre requête. Nous ne garantissons pas que le fichier résultant sera cohérent entre les deux exports qui y sont combinés.'."\n");
 				$re = true;
 				break;
