6b51c2c5d01fd79d6d2b9f1e33bf892ce89e7b5e
Guillaume Outters <guillaume-sqleur@outters.eu>
2020-09-25 02:23:03 +0200

    tests/for
    = Petits tests pour les boucles #for et le découpage des listes sur lesquelles boucler.
    
    darcs-hash:7478e2e08e4108803459c24822907e6ba138f925

---
+++ tests/for.test.res.sql
@@ -0,0 +1,21 @@
+create table temp_premier;
+
+OUI1 premier;
+
+create table temp_second;
+
+OUI2 second;
+
+select id
+	, A
+	, B
+	, C
+	, D
+from toto;
+
+select
+Paquet:
+a b c
+Paquet:
+d e f
+;
---
+++ tests/for.test.sql
@@ -0,0 +1,30 @@
+-- prepro SqleurPreproIncl SqleurPreproDef
+
+#for TRUC in "premier" "second"
+
+create table temp_TRUC;
+
+#if TRUC == "premier"
+OUI1 TRUC;
+#elif TRUC == "second"
+OUI2 TRUC;
+#else
+[31mNON[0m TRUC;
+#endif
+
+#done
+
+select id
+#for CHAMP in A B C D
+	, CHAMP
+#done
+from toto;
+
+select
+#for TRUC in "a b c" "d e f"
+Paquet:
+#for MACHIN in TRUC
+MACHIN
+#done
+#done
+;
