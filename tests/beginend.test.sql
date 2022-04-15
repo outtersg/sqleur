31819aa2707291b415d684447706d7e7fee9e2e3
Guillaume Outters <guillaume-sqleur@outters.eu>
2022-04-16 00:14:32 +0200

    tests/beginend.test.sql: begin transaction
    = Ne suit pas du tout la même route que begin end;.
    
    darcs-hash:421e1dac169b9038ae9a2a47eeeee18b35198310

--- tests/beginend.test.sql
+++ tests/beginend.test.sql
@@ -32,3 +32,12 @@ begin case when truc then rend les endives else machin end; end;
 #testdecoupe create trigger truc begin case when truc then endbegin else machin end; end
 create trigger truc
 begin case when truc then endbegin else machin end; end;
+
+-- Ne pas confondre avec un begin transaction! Cette fois-ci on ne recherche pas le end.
+#testdecoupe begin transaction
+begin transaction;
+commit;
+-- Même chose pour le begin sans rien derrière, synonyme de begin transaction chez PostgreSQL. C'est le ";" immédiat qui fait la différence.
+#testdecoupe begin
+begin;
+commit;
