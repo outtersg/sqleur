a1b26f6df72dcc266539d36ad63e2bb5b744d41e
Guillaume Outters <guillaume-sqleur@outters.eu>
2022-04-16 00:15:18 +0200

    tests/cesures.test.sql: MODE_BEGIN_END
    = Tests tordus sur une césure entre le begin et le transaction (qui en change le sens).
    
    darcs-hash:04ca5a4ccabbc751d9e0556c21b913ae28e49edc

--- tests/cesures.test.res.sql
+++ tests/cesures.test.res.sql
@@ -1,3 +1,9 @@
-select blablabla bliblibli from youpilouplaboumdinglagadatsointsoin;
+select blablabla from youpilouplaboum;
 
 blorp;
+
+begin transaction;
+commit;
+
+select remplissage pour le plaisir;
+begin toto; end;
--- tests/cesures.test.sql
+++ tests/cesures.test.sql
@@ -1,5 +1,7 @@
 -- prepro SqleurPreproDef
--- sqleur.tailleBloc 400
+-- sqleur._mode MODE_BEGIN_END
+-- sqleur.tailleBloc 100
+
 -- /!\ Le test a pour but de vérifier comment est traité le #if s'il tombe à cheval sur une limite de bloc.
 -- Il faut donc qu'un dd if=cefichier.sql bs=400 count=1 renvoie un machin coupé pile entre "#if GL" et "OUB".
 
@@ -7,10 +9,21 @@
 #define GLOUB 1
 #endif
 
-select blablabla bliblibli from youpilouplaboumdinglagadatsointsoin;
+select blablabla from youpilouplaboum;
 
 #if GLOUB
 blorp;
 #else
 blirp;
 #endif
+
+-- begin transaction avec espace pile sur la césure.
+-- Est-il bien interprété comme un begin transaction (instruction SQL autonome) et non un begin (attendant un end)?
+-- Le "; commit" est restitué en ";\ncommit" si Sqleur détecte bien les 2 instructions.
+
+begin transaction; commit;
+
+-- Un vrai begin end;, cette fois:
+
+select remplissage pour le plaisir;
+begin toto; end;
