7731538eb902cdf97f7eff5273061c773dbb1eca
Guillaume Outters <guillaume-sqleur@outters.eu>
2022-09-09 00:25:59 +0200

    tests/beginendpointvirgule: en fait Oracle demande des points-virgule sur tous les blocs
    = Contrairement à ce que je pensais initialement, le point-virgule est aussi requis dans les blocs "create function as begin machin; end;" que dans les begin / end anonymes (hors définition de fonction).
    
    darcs-hash:ab9408ed3a69b9838886b4f0b91e5f4660e09f46

--- tests/beginendpointvirgule.test.res0.sql
+++ tests/beginendpointvirgule.test.res0.sql
@@ -24,18 +24,18 @@ for machin in truc loop coucou--#
 end loop--#
 
 -- Mic-mac entre déclarations de fonctions à end et sans end:
-create function bla() return integer as begin select 1 from dual order by case when 1 = 1 then 1 end; end--#
-create function bla() return integer as maVar integer; begin select 1 from dual order by case when 1 = 1 then 1 end; end--#
+create function bla() return integer as begin select 1 from dual order by case when 1 = 1 then 1 end; end;--#
+create function bla() return integer as maVar integer; begin select 1 from dual order by case when 1 = 1 then 1 end; end;--#
 create function bla() return integer
 as
 	maVar integer;
 begin
 	select 1 from dual order by case when 1 = 1 then 1 end;
-end--#
+end;--#
 create function bla() return integer as $$ select 1; $$--#
 create function bla() return integer as
 $$
 	select 1;
 $$--#
 create table bla as select 1--#
-create package machin as function bla() as maVar integer; begin coucou; end; end machin--#
+create package machin as function bla() as maVar integer; begin coucou; end; end machin;--#
