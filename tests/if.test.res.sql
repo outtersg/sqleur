4a0ab6fa5bccdd679cc6c0aa302fd3c42b294e78
Guillaume Outters <guillaume-sqleur@outters.eu>
2022-09-10 23:27:05 +0200

    tests: if.test.res.sql, while.test.res.sql
    = Profitons de ce que test.php compare de façon plus limpide pour automatiser le scrutin du résultat de test sur deux tests historiques bêtes mais chiants à vérifier.
    
    darcs-hash:66d5ac914de7bb47defe470c4f9b49f1d887b4d8

---
+++ tests/if.test.res.sql
@@ -0,0 +1,10 @@
+L''instruction suivante doit contenir 4 mots OUI et aucun NON.;
+select
+$$
+================
+Je suis l''instruction suivante et [32mOUI[90m, je contiens
+[32mOUI[90m
+[32mOUI[90m
+et ma fin, [32mOUI[90m.
+================
+$$;
---
+++ tests/while.test.res.sql
@@ -0,0 +1,8 @@
+Appelé avec un petit 0
+Appelé avec un petit 1
+Appelé avec un grand 2
+
+compteur et multiplieur = [32m3 8[0m;
+
+Boucle for = [32m6[0m;
+Boucle for = [32mabc[0m;
