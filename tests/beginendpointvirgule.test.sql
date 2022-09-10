b6f43f49a1760bb926cd11283725ce7de2341116
Guillaume Outters <guillaume-sqleur@outters.eu>
2022-09-10 23:28:31 +0200

    tests: beginendpointvirgule.test.sql: sans shell
    - Maintenant que le résultat est vérifié en standard par test.php, plus besoin de mettre en commentaire le mode op' shell de test.
    
    darcs-hash:319e2b7309895b85d27915a7f7a493b946b12182

--- tests/beginendpointvirgule.test.sql
+++ tests/beginendpointvirgule.test.sql
@@ -1,16 +1,6 @@
 -- Ce test est un peu particulier car il teste l'embarquement du ; final dans le SQL sorti;
 -- or ce ; est fusionné avec celui de séparation de requêtes: pour distinguer les deux il faut passer en mode -0.
--- Donc test en pur shell:
-#if 0
-esp="`printf '\100'`" &&
-cat tests/beginendpointvirgule.test.res0.sql > /tmp/1 &&
-php sql2csv.php -E -0 tests/beginendpointvirgule.test.sql | tr '\000' $esp | sed -e "s/$esp/--#$esp/g" | tr $esp '\012' > /tmp/2 &&
-for f in 1 2
-do
-	sed -e 's/ *--[^#].*//' -e '/^$/d' < /tmp/$f > /tmp/$f.propre
-done &&
-diff -uw /tmp/2.propre /tmp/1.propre
-#endif
+-- Donc test en .res0 (cf. le .res0.sql).
 
 -- À la SQL*Plus:
 begin
