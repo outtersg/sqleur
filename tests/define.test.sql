35efcd27c60d0acf1446754450c1d1bbde8a42a5
Guillaume Outters <guillaume-sqleur@outters.eu>
2024-02-11 22:49:18 +0100

    tests/define: paramètres vides
    = On fait remarquer par les tests qu'un paramètre vide est compté comme paramètre, la virgule suffisant à le matérialiser.
    # Aïe, mais au passage on se rend compte que si TITI() et TITI(x) sont définis, l'appel de TITI() incluera… le second de préférence au premier!
    
    darcs-hash:898ed34622014fa0bf1eb1cac9a6b6b72bc62bc5

--- tests/define.test.sql
+++ tests/define.test.sql
@@ -17,3 +17,21 @@ TOTO(a, b, c);
 
 #testdecoupe a + b, c + xy
 TOTO(a, b\, c);
+
+-- Les paramètres sont autorisés vides.
+#testdecoupe a + + xy
+TOTO(a,);
+#testdecoupe + + xy
+TOTO(,);
+
+-- … Sauf le premier (car sinon comment distinguer une fonction à 0 paramètres d'une à 1?
+-- Ouille en fait ben l'implémentation originale se plante :-\ Si une fonction possède deux variantes, avec 0 et 1 paramètre, l'appeler avec 0 invoquera celle à 1 paramètre en le lui fournissant vide. Grrr…
+-- À FAIRE: implémenter proprement. Mais sans doute dans une version majeure.
+--#define UN(x) un x
+----#define UN() zéro x
+--#testdecoupe UN()
+--UN();
+--#testdecoupe un a
+--UN(a);
+--#testdecoupe UN(a,)
+--UN(a,);
