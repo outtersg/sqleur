4f51bfa474c63e324c960b718371ee612f6a3078
Guillaume Outters <guillaume-sqleur@outters.eu>
2024-05-09 22:12:06 +0200

    Prepro: mutu: _préempterSql()
    - Depuis le temps que je copie-colle l'absorption de requêtes SQL par une instruction préproc…
    
    darcs-hash:ad0ed1116ae3e6547db6619884191ff3198923e0

--- SqleurPrepro.php
+++ SqleurPrepro.php
@@ -8,6 +8,37 @@ class SqleurPrepro
 	{
 		$this->_sqleur = $sqleur;
 	}
+	
+	/*- Préemption -----------------------------------------------------------*/
+	
+	/**
+	 * Dérive le Sqleur pour récupérer les $n prochaines requêtes SQL.
+	 */
+	protected function _préempterSql($n = 1)
+	{
+		$this->_nReqsÀChoper = $n;
+		$this->_sortieOriginelle = $this->_sqleur->_sortie;
+		
+		$foncEtParams = func_get_args();
+		array_splice($foncEtParams, 0, 1, [ $this, '_préemptionSql' ]);
+		$this->_sqleur->_sortie = $foncEtParams;
+	}
+	
+	public function _préemptionSql($req)
+	{
+		if(!--$this->_nReqsÀChoper)
+		{
+			/* BOGUE: un #create récoltant les requêtes entrecoupées d'#if ne se restaure pas bien et continue à choper après. */
+			$this->_sqleur->_sortie = $this->_sortieOriginelle;
+			unset($this->_sortieOriginelle);
+		}
+		
+		$params = func_get_args();
+		return call_user_func_array([ $this, '_chope' ], $params);
+	}
+	
+	protected $_sortieOriginelle;
+	protected $_nReqsÀChoper;
 }
 
 ?>
