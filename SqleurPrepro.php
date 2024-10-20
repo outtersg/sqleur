9714a0e87048fba5242cbd0b7261730969240888
Guillaume Outters <guillaume-sqleur@outters.eu>
2024-10-21 00:55:15 +0200

    Prepro: _préempterSql(TERMINE) == _préempterSql <<TERMINE
    + De quoi gérer le HereDoc dans _préempterSql() (le préprocesseur souhaite détourner les n prochaines requêtes), sans connaître n mais en ayant une idée de la balise de fin.
    
    darcs-hash:8fb7e06fbfdcb533c2d013915f63f9aa7b1b52a6

--- SqleurPrepro.php
+++ SqleurPrepro.php
@@ -13,6 +13,9 @@ class SqleurPrepro
 	
 	/**
 	 * Dérive le Sqleur pour récupérer les $n prochaines requêtes SQL.
+	 * 
+	 * @param int|string $n Nombre de requêtes à manger, ou terminateur façon HereDoc (une requête constituée du terminateur clôt la série).
+	 *                      Dans ce cas, un dernier _chope(null) sera émis pour signifier la complétude.
 	 */
 	protected function _préempterSql($n = 1)
 	{
@@ -26,11 +29,21 @@ class SqleurPrepro
 	
 	public function _préemptionSql($req)
 	{
-		if(!--$this->_nReqsÀChoper)
+		if(is_numeric($this->_nReqsÀChoper))
+			--$this->_nReqsÀChoper;
+		else if($req === $this->_nReqsÀChoper)
+			$this->_nReqsÀChoper = null;
+		if($this->_nReqsÀChoper === 0 || $this->_nReqsÀChoper === null)
 		{
 			/* BOGUE: un #create récoltant les requêtes entrecoupées d'#if ne se restaure pas bien et continue à choper après. */
 			$this->_sqleur->_sortie = $this->_sortieOriginelle;
 			unset($this->_sortieOriginelle);
+			
+			if($this->_nReqsÀChoper === null)
+			{
+				$this->_chope(null);
+				return;
+			}
 		}
 		
 		$params = func_get_args();
